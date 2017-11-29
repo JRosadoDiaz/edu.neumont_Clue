98f316c234760064eef10d654ab384ea4ad60985
Sheadra <vokuungrohiik@gmail.com>
2017-11-29 13:41:30 -0700

    i did the arraylist you wanted in RoomLogic

--- New folder/src/rosado/jose/csc110/RoomLogic.java
+++ New folder/src/rosado/jose/csc110/RoomLogic.java
@@ -1,9 +1,13 @@
 package rosado.jose.csc110;
 
+import java.util.ArrayList;
+
 public class RoomLogic {
 	/*
 	 * this class needs to have the position of the rooms on the 2d array
 	 * track when players enter a room
 	 * hold a players options while in a room
 	 */
+	ArrayList<Player> players = new ArrayList<>(); 
+	
 }
