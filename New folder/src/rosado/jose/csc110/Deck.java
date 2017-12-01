c5f6002d1f4fda421c71382926bc5770a11f38b0
Sheadra <vokuungrohiik@gmail.com>
2017-12-01 15:25:30 -0700

    added the methods to set the cards that go into the confidential case file

--- New folder/src/rosado/jose/csc110/Deck.java
+++ New folder/src/rosado/jose/csc110/Deck.java
@@ -1,6 +1,7 @@
 package rosado.jose.csc110;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
 	
@@ -21,7 +22,67 @@ public class Deck {
 	 * Those chosen cards will be removed and the remaining cards...
 	 * ...will be shuffled to the players into a new list called hand
 	 */
-	ArrayList<Suspects> suspectCards = new ArrayList<>();
-	ArrayList<Weapons> weaponCards = new ArrayList<>();
-	ArrayList<Rooms> roomCards = new ArrayList<>();
+	
+	private Random gen = new Random();
+	
+	private ArrayList<Enum> list = new ArrayList<>();
+	
+	public ArrayList<Enum> confidential = new ArrayList<>();	
+	
+	private ArrayList<Suspects> suspectCards = new ArrayList<>();
+	private void setSuspectCards() {
+		suspectCards.add(Suspects.Miss_Scarlet);
+		suspectCards.add(Suspects.Mrs_White);
+		suspectCards.add(Suspects.Mrs_Peacock);
+		suspectCards.add(Suspects.Colonel_Mustard);
+		suspectCards.add(Suspects.Professor_Plum);
+		suspectCards.add(Suspects.Mr_Green);
+	}
+	
+	private ArrayList<Weapons> weaponCards = new ArrayList<>();
+	private void setWeaponCards() {
+		weaponCards.add(Weapons.Rope);
+		weaponCards.add(Weapons.LeadPipe);
+		weaponCards.add(Weapons.Knife);
+		weaponCards.add(Weapons.Wrench);
+		weaponCards.add(Weapons.CandleStick);
+		weaponCards.add(Weapons.Pistol);
+	}
+	
+	private ArrayList<Rooms> roomCards = new ArrayList<>();
+	private void setRoomCards() {
+		roomCards.add(Rooms.Study);
+		roomCards.add(Rooms.Hall);
+		roomCards.add(Rooms.Lounge);
+		roomCards.add(Rooms.Library);
+		roomCards.add(Rooms.DiningRoom);
+		roomCards.add(Rooms.BilliardRoom);
+		roomCards.add(Rooms.Conservatory);
+		roomCards.add(Rooms.BallRoom);
+		roomCards.add(Rooms.Kitchen);
+	}
+	
+	
+	public void choseConfidential() {
+		setSuspectCards();
+		setWeaponCards();
+		setRoomCards();
+		setConfidentialCards(generateNumber(suspectCards), suspectCards);
+		setConfidentialCards(generateNumber(weaponCards), weaponCards);
+		setConfidentialCards(generateNumber(roomCards), roomCards);
+	}
+	
+	private int generateNumber(ArrayList separateList) {
+		return gen.nextInt(separateList.size());
+	}
+	
+	private void setConfidentialCards(int pew, ArrayList cool) {
+		confidential.add((Enum)cool.get(pew));
+	}
+	
+	public void setFullList() {
+		list.addAll(suspectCards);
+		list.addAll(weaponCards);
+		list.addAll(roomCards);
+	}
 }
